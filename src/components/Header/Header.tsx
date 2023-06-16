a7c46743b83238d32d60005372f4ed33ead18ebe
udaysarkarud <udaysarkarud@gmail.com>
2023-06-16 15:59:27 +0600

    initial commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,17 @@
+import "./Header.css";
+import logo from "./../../images/Logo.svg";
+const Header = () => {
+  return (
+    <nav className="header">
+      <img src={logo} />
+      <div>
+        <a href="/shop">shop</a>
+        <a href="/order">order</a>
+        <a href="/inventory">inventory</a>
+        <a href="/login">login</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
